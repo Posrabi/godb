ef702e0481233e601eab67e3451bc3bcae06d9bb
tridao <daominhtri0503@gmail.com>
2022-05-16 20:49:05 -0400

    simple ast model

---
+++ src/ast.go
@@ -0,0 +1,51 @@
+package src
+
+type ast struct {
+	Statements []*statement
+}
+
+type astKind uint
+
+const (
+	SelectAstKind astKind = iota
+	CreateAstKind
+	InsertAstKind
+)
+
+type statement struct {
+	Select *selectStatement
+	Create *createStatement
+	Insert *insertStatement
+	Kind   astKind
+}
+
+type insertStatement struct {
+	table  token
+	values *[]*expression
+}
+
+type expressionKind uint
+
+const (
+	literal expressionKind = iota
+)
+
+type expression struct {
+	literal *token
+	kind    expressionKind
+}
+
+type columnDefinition struct {
+	name     token
+	dataType token
+}
+
+type createStatement struct {
+	name token
+	col  *[]*columnDefinition
+}
+
+type selectStatement struct {
+	item []*expression
+	from token
+}
